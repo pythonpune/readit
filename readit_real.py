6db0bfc25af3a4a169950baf4302add375145a9f
Ganesh Hubale <ganeshhubale03@gmail.com>
2017-12-20 14:50:43 +0530

    added request

--- readit_real.py
+++ readit_real.py
@@ -3,6 +3,7 @@ import psycopg2
 import sys
 import datetime
 import os
+import requests
 
 class DatabaseConnection(object):
 
@@ -68,7 +69,12 @@ if __name__== '__main__':
     args = parser.parse_args()
     
     if args.add:
-        database_connection.insert_new_record(args.add)
+        urldata = str(args.add)
+        url = requests.get(urldata)
+        if (404==url.status_code):
+            print("Invalid URL...")
+        else:
+            database_connection.insert_new_record(args.add)
     
     elif args.view:
         database_connection.query_all()
@@ -85,11 +91,14 @@ if __name__== '__main__':
 
         database_connection.drop_table()
 
-    elif args.create:                                                                                                                                        database_connection.create_table()
+    elif args.create:
+        
+        database_connection.create_table()
 
     else:
-	
-        database_connection.insert_new_record(args.add)
 
+        print("HELP -ct")
+	
+    
 
 
