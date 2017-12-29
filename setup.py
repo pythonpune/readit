0baa2546eccdc4b044a3595ceaf6aaac9a7f836e
Ganesh Hubale <ganeshhubale03@gmail.com>
2017-12-30 03:19:53 +0530

    set up updated

--- setup.py
+++ setup.py
@@ -10,13 +10,6 @@ install_requires = [
     
     ]
 
-#tests_require = [
-    # See tox.iniq
- #   'pytest >=2.8.3',
-   # 'coverage',
-  #  ]
-
-
 setup(
     name="readit",
     version="0.1.0",
@@ -30,42 +23,11 @@ setup(
     py_modules=[],
     packages=['manager', ],
     namespace_packages=[],
-    long_description=read('README.md'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: End Users/Developers",
-        "Intended Audience :: System Administrators",
-        "Topic :: Utilities",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        (
             
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
     include_package_data=True,
     zip_safe=False,
-   # setup_requires=setup_requires,
+ 
     install_requires=install_requires,
-   # tests_require=tests_require,
-  #  extras_require=dict(
-   #     tests=tests_require,
-    #    docs=docs_require,
-     #   ),
-      # add more sources of randomness here...
- #  },
-    entry_points='''
-        [console_scripts]
-        manager.readit=readit:main()
-        ''',
-)
+    )
+ 
+
