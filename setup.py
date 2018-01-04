6a6724dfcdc02a03f65e5d4979c89d0c8a6c1b3d
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-01-04 19:59:02 +0530

    entry points added

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 
 from setuptools import setup, find_packages
 
-
+with open("README") as f:
+    long_description = f.read()
 
 install_requires = [
     'setuptools',
@@ -12,11 +13,16 @@ install_requires = [
 
 setup(
     name="readit",
+    entry_points = {
+        'console_scripts': [
+            'readit = __init__.py:main',
+            ],
+        },
     version="0.1",
     author="Ganesh, Shital, Daivshala",
     author_email="ganeshhubale03@gmail.com",
-    description=(
-        "It is bookmark manager."),
+    description="It is bookmark manager.",
+    long_description = long_description,
     license="MIT",
     keywords="clitool bookmark readit",
     url="https://github.com/ganeshhubale/readit",
