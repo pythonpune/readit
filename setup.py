3ee815a9ca7b27d815c7f289b5ac810c1e9e052b
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-01-17 19:55:05 +0530

    module added

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     license="GNU General Public License v3.0",
     keywords="clitool bookmark readit",
     url="https://github.com/ganeshhubale/readit",
-    py_modules=[],
+    py_modules=[manager,],
     namespace_packages=[],
             
     include_package_data=True,
