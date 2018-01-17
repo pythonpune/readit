48c9aa7de8fd5ef85e60e506101e9037e07840bd
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-01-17 23:22:21 +0530

    doc string added

--- manager/database.py
+++ manager/database.py
@@ -6,10 +6,10 @@ class DatabaseConnection(object):
     def __init__(self, cursor, db):
         
         """
-        Create databse connection
-        creates or opens file mydatabase with sqlite3 DataBase
-        get cursor object
-        create table
+        Create database connection.
+        creates or opens file mydatabase with sqlite3 DataBase.
+        get cursor object.
+        create table.
         """
         
         try:
@@ -23,27 +23,35 @@ class DatabaseConnection(object):
             self.db.commit()
 
         except sqlite3.OperationalError:
+            
             print("Table coulden't be created:")
     
     def add_url(self, url):
+        """
+        url will be adding to database.
+        """
         self.url = url
-        print(self.url)
          
         self.cursor.execute('''
         INSERT INTO bookmarks(url) VALUES (?)
         ''', (self.url,))
-        print("inserted")
         self.db.commit()
        # self.db.close()
     
     def delete_url(self, urlid):
+        """
+        url can deleted as per id number provided.
+        """
         self.urlid = urlid
-        print(self.urlid)
         self.cursor.execute(''' DELETE FROM bookmarks WHERE id=? ''', (self.urlid,))
         self.db.commit()
         #self.db.close()
     
     def update_url(self, url):
+        """
+        url can be updated with respect to id.
+        """
+
         self.url = url
         self.arr[2] = self.url
         urlid = arr[0]
@@ -53,11 +61,15 @@ class DatabaseConnection(object):
         #self.db.close()
 
     def show_url(self):
+        """
+        all urls from database shown to user on screen.
+        """
+
         self.cursor.execute(''' SELECT id, url FROM bookmarks ''')
         all_row = self.cursor.fetchall()
         for row in all_row:
             print(row)
-        print("data show succes")
+      #  print("data show succes")
         self.db.commit()
         #self.db.close()
 
