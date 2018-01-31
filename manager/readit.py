f97c1efc15b80c1e78b314831b2c560b4c23f0e4
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-01-31 18:08:15 +0530

    Add command line options '--tag', '--search'

--- manager/readit.py
+++ manager/readit.py
@@ -6,12 +6,14 @@ import database as db  # used to perform database operations.
 
 @click.command()
 @click.option('--add', '-a', multiple=True, help="Add URLs")
+@click.option('--tag', '-t', multiple=True, help="Add URL with tag")
 @click.option('--delete', '-d', multiple=True, help="Delete a URL")
-@click.option('--clear', '-c', multiple=True, nargs=0, help="Clear database")
-@click.option('--update', '-u', multiple=True,  help="Update a URL")
-@click.option('--view', '-v', multiple=True, nargs=0,  help="Show URLs")
+@click.option('--clear', '-c', multiple=True, nargs=0, help="Clear bookmarks")
+@click.option('--update', '-u', multiple=True, help="Update a URL")
+@click.option('--search', '-s', multiple=True, help="Search all URLs by Tag")
+@click.option('--view', '-v', multiple=True, nargs=0, help="Show URLs")
 @click.argument('insert', nargs=-1, required=False)
-def main(insert, add, delete, clear, update, view):
+def main(insert, add, tag, delete, clear, update, search, view):
     """
     It performs database operations as per arguments passed by user.
     """
@@ -55,8 +57,29 @@ def main(insert, add, delete, clear, update, view):
 
     elif view:
         d.show_url()
+    elif search:
+        taglist = []
+        for i in search:
+            taglist.append(i)
+        for j in taglist:
+            d.search_by_tag(j)
     elif clear:
         d.delete_all_url()
+    elif tag:
+        taglist = []
+        for i in tag:
+            taglist.append(i)
+        tag_name = taglist[0]
+        tagged_url = taglist[1]
+        try:
+            u = requests.get(tagged_url)
+            r = u.status_code
+            if r == 200:
+                d.tag_url(tag_name, tagged_url)
+            else:
+                print("Invalid URL:-->", tagged_url)
+        except Exception as t:
+            print("Exception Caught:--> ", t)
     else:
         for i in insert:
             url = i
