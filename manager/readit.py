8e398a5ec464f38d5eb6bb96b75cd2cea0e9f5b7
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-02-01 20:11:36 +0530

    Remove pep8 standard error

--- manager/readit.py
+++ manager/readit.py
@@ -11,7 +11,7 @@ import database as db  # used to perform database operations.
 @click.option('--clear', '-c', multiple=True, nargs=0, help="Clear bookmarks")
 @click.option('--update', '-u', nargs=2, help="Update a URL for specific ID")
 @click.option('--search', '-s', nargs=1, help="Search all bookmarks by Tag")
-@click.option('--view', '-v', multiple=True, nargs=0, help="Show all bookmarks")
+@click.option('--view', '-v', multiple=True, nargs=0, help="Show bookmarks")
 @click.option('--version', '-V', is_flag=True, help="Check latest version")
 @click.argument('insert', nargs=-1, required=False)
 def main(insert, add, tag, delete, clear, update, search, view, version):
