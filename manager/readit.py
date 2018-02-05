435f6ca2e9d95241e625fa56ade743ae0e3ef742
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-02-05 22:30:46 +0530

    Resolved conflict in readit file

--- manager/readit.py
+++ manager/readit.py
@@ -1,7 +1,7 @@
 
 import requests  # to check whether url is valid or not
 import click  # used for command line interface.
-import database as db  # used to perform database operations.
+import database  # used to perform database operations.
 
 
 @click.command()
@@ -18,71 +18,71 @@ def main(insert, add, tag, delete, clear, update, search, view, version):
     """
     Readit - Command-line bookmark manager tool.
     """
-    d = db.DatabaseConnection()
+    object_database = database.DatabaseConnection()
 
     if add:
-        for i in add:
-            url = i
+        for url_to_add in add:
+            url = url_to_add
             try:
-                u = requests.get(url)
-                r = u.status_code
-                if r == 200:
-                    d.add_url(url)
+                validate_url = requests.get(url)
+                validate_code = validate_url.status_code
+                if validate_code == 200:
+                    object_database.add_url(url)
                 else:
                     print("Invalid URL:--> ", url)
             except Exception as e:
                 print("Invalid input:--> ", e)
 
     elif delete:
-        d.delete_url(delete)
+        object_database.delete_url(delete)
 
     elif update:
-        mylist = []
-        for i in update:
-            mylist.append(i)
-        uid = mylist[0]
-        url = mylist[1]
+        url_list = []
+        for update_to_url in update:
+            url_list.append(update_to_url)
+        url_id = url_list[0]
+        url = url_list[1]
         try:
-            u = requests.get(url)
-            r = u.status_code
-            if r == 200:
-                d.update_url(uid, url)
+            validate_url = requests.get(url)
+            validate_code = validate_url.status_code
+            if validate_code == 200:
+                object_database.update_url(url_id, url)
             else:
                 print("Invalid URL:--> ", url)
         except Exception as e:
             print("Invalid input:-->  ", e)
 
     elif view:
-        d.show_url()
+        object_database.show_url()
     elif search:
-        d.search_by_tag(search)
+        object_database.search_by_tag(search)
     elif clear:
-        d.delete_all_url()
+        object_database.delete_all_url()
     elif tag:
-        taglist = []
-        for i in tag:
-            taglist.append(i)
-        tag_name = taglist[0]
-        tagged_url = taglist[1]
+        tag_list = []
+        for tag_to_url in tag:
+            tag_list.append(tag_to_url)
+        tag_name = tag_list[0]
+        tagged_url = tag_list[1]
         try:
-            u = requests.get(tagged_url)
-            r = u.status_code
-            if r == 200:
-                d.tag_url(tag_name, tagged_url)
+            validate_url = requests.get(tagged_url)
+            validate_code = validate_url.status_code
+            if validate_code == 200:
+                object_database.tag_url(tag_name, tagged_url)
             else:
                 print("Invalid URL:-->", tagged_url)
         except Exception as t:
             print("Invalid input:--> ", t)
     elif version:
-        print("readit 1.0")
+        print("readit v1.0")
     else:
-        for i in insert:
-            url = i
+        for url_to_add in insert:
+            url = url_to_add
             try:
-                u = requests.get(url)
-                r = u.status_code
-                if r == 200:
-                    d.add_url(url)
+                validate_url = requests.get(url)
+                validate_code = validate_url.status_code
+                if validate_code == 200:
+                    object_database.add_url(url)
                 else:
                     print("Invalid URL:--> ", url)
             except Exception as e:
