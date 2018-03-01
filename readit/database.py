ef3b9d0de2a66dc684d04458b062a4f88a3f9b4e
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-03-01 16:47:07 +0530

    Add fixed path to database file (#64)
    * Add fixed path to database file
    
    * Made use of 'path.join' to create path to database file

--- readit/database.py
+++ readit/database.py
@@ -2,6 +2,7 @@ import sqlite3  # library of database used for project
 import datetime  # used for getting current time and date
 from beautifultable import BeautifulTable  # display output in table format
 import webbrowser  # used to open url in browser
+import os  # used to find home directory of user
 
 date = datetime.date.today()
 
@@ -30,7 +31,16 @@ class DatabaseConnection(object):
         """
 
         try:
-            self.db = sqlite3.connect('test.db')
+            config_path = os.path.join(
+                os.path.expanduser('~'), ".config/readit")
+            if not os.path.exists(config_path):
+                os.mkdir(config_path)
+        except OSError:
+            print('Error: Creating directory.' + config_path)
+
+        databasefile = os.path.join(config_path, "bookmarks.db")
+        try:
+            self.db = sqlite3.connect(databasefile)
             self.cursor = self.db.cursor()
             self.cursor.execute('''CREATE TABLE IF NOT EXISTS bookmarks
             (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
