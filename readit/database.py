8b9f0921d56f692826cbd54e494f43e6163b2256
Ganesh Hubale <ganeshhubale03@gmail.com>
2018-04-13 09:44:20 +0530

    pep8 error resolved

--- readit/database.py
+++ readit/database.py
@@ -406,7 +406,7 @@ class DatabaseConnection(object):
                 ''' SELECT id, url, tags, date, time
                             FROM bookmarks WHERE url=?''', (self.url_exist,))
             all_bookmarks = self.cursor.fetchall()
-            print("*" * 31,"\nThis URL is already bookmarked.\n", "*" * 30)
+            print("*" * 31, "\nThis URL is already bookmarked.\n", "*" * 30)
             for bookmark in all_bookmarks:
                 table.append_row(
                     [bookmark[0], bookmark[1], bookmark[2],
