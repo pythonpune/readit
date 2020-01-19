fc9d3c794b9f8864fc487f5e580fda89c49a9a84
Nikhil Dhandre <nik.digitronik@live.com>
2020-01-19 10:13:08 +0530

    introduce setup.cfg for metadata

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-with open("README.rst") as readme_file:
-    long_description = readme_file.read()
-
-install_requires = ["setuptools", "requests", "click", "beautifultable"]
-
-setup(
-    name="readit",
-    packages=find_packages(),
-    entry_points={"console_scripts": ["readit = readit.__init__:main"]},
-    version="0.2",
-    author="Ganesh, Shital, Daivshala",
-    author_email="ganeshhubale03@gmail.com",
-    description="Readit - Command Line Bookmark Manager Tool",
-    long_description=long_description,
-    license="GNU General Public License v3.0",
-    keywords="clitool bookmark readit",
-    url="https://github.com/projectreadit/readit",
-    py_modules=["readit.__init__"],
-    namespace_packages=[],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    scripts=["test_readit.py"],
-)
+setup()
